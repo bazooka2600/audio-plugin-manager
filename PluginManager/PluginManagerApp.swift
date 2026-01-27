251c7725a9462cedb45c6704bd83c820870fbe17
bazooka <bazooka@bazooka.systems>
2026-01-27 18:20:14 -0500

    Initial Commit

---
+++ PluginManager/PluginManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PluginManagerApp.swift
+//  PluginManager
+//
+//  Created by Admin on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct PluginManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
